fa6460a5196fa0287e86657b3d550980c92cb356
lucasliet <lucasouliveira@gmail.com>
2020-06-30 05:03:30 -0300

    💅 Issues Page styles

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
 
-import { Title } from './styles';
+import { RepoInfo, Header, Issues } from './styles';
 
-const Repo: React.FC = () => <Title>Workou o Repo Carai!</Title>;
+import logo from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Repo: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+      <RepoInfo>
+        <header>
+          <img />
+          <div>
+            <strong />
+            <p />
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong />
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong />
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong />
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepoInfo>
+
+      <Issues>
+        <Link to="t">
+          <div>
+            <strong>name</strong>
+            <p>t</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,6 +1,120 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 28px;
-  color: #3a3a3a;
+export const RepoInfo = styled.section`
+  margin-top: 50px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+    div {
+      margin-left: 24px;
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #737380;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+      strong {
+        display: block;
+        font-size: 34px;
+        color: #3d3d4d;
+      }
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c88;
+      }
+    }
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color ease-in 0.3s;
+    &:hover {
+      color: #555;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+  }
+`;
+
+export const Issues = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-top: 80px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    background: white;
+    border-radius: 5px;
+
+    width: 100%;
+    padding: 24px;
+
+    text-decoration: none;
+
+    transition: transform ease-in-out 0.2s;
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    div {
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #303d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
