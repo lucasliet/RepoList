9a2290131380c1377046f64e588aeb5989af1403
lucasliet <lucasouliveira@gmail.com>
2020-06-30 05:54:55 -0300

    Updates

---
+++ precache-manifest.b75dfea06bd3baef84558b059de4f7e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a495d20cc168878042b028c6d78237",
+    "url": "/RepoList/index.html"
+  },
+  {
+    "revision": "6d0a0c79f27f2772674c",
+    "url": "/RepoList/static/js/2.fc12adf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RepoList/static/js/2.fc12adf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b70bfef60568ea335f",
+    "url": "/RepoList/static/js/main.3641de96.chunk.js"
+  },
+  {
+    "revision": "86b21588f68804268d87",
+    "url": "/RepoList/static/js/runtime-main.795fd07f.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/RepoList/static/media/background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/RepoList/static/media/logo.04f6798f.svg"
+  }
+]);
