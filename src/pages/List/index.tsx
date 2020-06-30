ed0e0c52e260dd9338ffe8799034e9ac41852d21
lucasliet <lucasouliveira@gmail.com>
2020-06-30 03:41:38 -0300

    🌲 root page structure styled

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, RepoList } from './styles';
 
-const List: React.FC = () => <Title>Workou Carai!</Title>;
+import logo from '../../assets/logo.svg';
+
+const List: React.FC = () => (
+  <>
+    <img src={logo} alt="Github Explorer" />
+    <Title>Digite o Repositório a ser buscado.</Title>
+    <Form>
+      <input type="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+    <RepoList>
+      <Link to="/repository">
+        <img />
+        <div>
+          <strong>Nome</strong>
+          <p>Descricao</p>
+        </div>
+        <FiChevronRight />
+      </Link>
+    </RepoList>
+  </>
+);
 
 export default List;
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -1,6 +1,99 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
+  margin-top: 60px;
   font-size: 28px;
   color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #4dd354;
+    width: 210px;
+    height: 70px;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: white;
+    font-weight: bold;
+    transition: background-color ease-in-out 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#4dd354')};
+    }
+  }
+`;
+
+export const RepoList = styled.div`
+  display: flex;
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    background: white;
+    border-radius: 5px;
+
+    width: 100%;
+    padding: 24px;
+
+    text-decoration: none;
+
+    transition: transform ease-in-out 0.2s;
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #303d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
